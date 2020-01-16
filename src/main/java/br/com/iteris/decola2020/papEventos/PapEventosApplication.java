b8999ca4b33228605dc2de5d9fb9c71e0a3684c8
Guilherme Sant'Anna <guilherme.santanna@iteris.com.br>
2020-01-16 12:45:59 -0300

    Initial commit

---
+++ src/main/java/br/com/iteris/decola2020/papEventos/PapEventosApplication.java
@@ -0,0 +1,13 @@
+package br.com.iteris.decola2020.papEventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PapEventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PapEventosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/iteris/decola2020/papEventos/PapEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.iteris.decola2020.papEventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PapEventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
