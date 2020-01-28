9a7847f347cc974a5393dd9fbc00c06bbf62d5a7
Guilherme Sant'Anna <guilherme.santanna@iteris.com.br>
2020-01-28 09:59:48 -0300

    Criação dos arquivos de configuração

---
+++ src/test/java/br/com/iteris/decola2020/papEventos/utils/IntegrationTestConfig.java
@@ -0,0 +1,9 @@
+package br.com.iteris.decola2020.papEventos.utils;
+
+/**
+ * IntegrationTestConfig
+ */
+public class IntegrationTestConfig {
+
+    public static final String appProperties = "classpath:application-integrationtests.properties";
+} 
